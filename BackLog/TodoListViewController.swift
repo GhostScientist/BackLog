60818a6cd316f91cf29518a8d6cdbc3ab67cb4db
Dakota Kim <S0281634@SalemState.edu>
2018-03-04 14:25:36 -0500

    Create add task functionality

--- BackLog/TodoListViewController.swift
+++ BackLog/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +44,29 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-
+    //MARK - Add new items to the list
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var taskDescription = UITextField()
+        
+        let alert = UIAlertController(title: "Add to BackLog", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
+            // what will happen once the user clicks the add task button on our alert.
+            self.itemArray.append(taskDescription.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new task"
+            taskDescription = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
